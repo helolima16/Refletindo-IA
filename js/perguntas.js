d4cb0f03bf8b34f41a14c2338ae930fe761cd5a9
helolima16 <126872091+helolima16@users.noreply.github.com>
2024-09-02 08:37:09 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,69 @@
+export const perguntas = [
+    {
+        enunciado: "Como a inteligência artificial está transformando o mercado de trabalho?",
+        alternativas: [
+            {
+                texto: "Reduzindo a demanda por empregos tradicionais",
+                afirmacao: [
+                    "Você achou que a demanda de empregos ficou escassa.",
+                    "Você percebeu que a oferta de empregos está limitada."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Criando novas oportunidades de emprego",
+                afirmacao: [
+                    "Você achou que é a criação de oportunidade de emprego.",
+                    "Você acredita que é a geração de novas oportunidades de trabalho."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais os principais desafios éticos associados ao uso crescente de inteligência artificial?",
+        alternativas: [
+            {
+                texto: "Melhora a precisão de dados.",
+                afirmacao: [
+                    "Vocẽ achou que se tem uma visão geral da precisão de dados.",
+                    "Você acredita que há uma perspectiva geral sobre a exatidão dos dado.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Aumentar a privacidade e segurança dos dados.",
+                afirmacao: [
+                    "Vocẽ achou que com a inteligẽncia artificial a privacidade dos dados é mais seguro.",
+                    "Você acredita que, com a inteligência artificial, a proteção da privacidade dos dados se tornou mais eficaz.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Na sua opinião, o desenvolvimento da IA deve ser",
+        alternativas: [
+            {
+                texto: "Rigorosamente regulamentado para evitar abuso",
+                afirmacao: [
+                    "O desenvolvimento da IA deve ser  impulsionado com a inovação nessa área e luta para abrir novos caminhos profissionais.",
+                    "O avanço da IA deve ser estimulado pela inovação e pela busca por novas oportunidades profissionais.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Liberado para incentivar a inovação",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "A Sua preocupação com as pessoas levou à criação de um grupo de estudos entre trabalhadores para explorar maneiras de utilizar a IA de forma ética..",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    
+    
+    
+   
+];
